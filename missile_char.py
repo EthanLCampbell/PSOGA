bee4640fc6dd7609fc1c404eaefffa6d5af597b8
Nick <nicholas.canovas@gmail.com>
2024-05-07 14:48:00 -0400

    Uploading main GA file and missile characteristics file

---
+++ missile_char.py
@@ -0,0 +1,32 @@
+from typing import List
+# Missle Family Chars
+
+missile_chars = namedtuple('missile_chars', ['name', 'value', 'weight'])
+
+missile_1 = [
+    missile_chars('traj',100,50),
+    missile_chars('cost', 100, 50),
+    missile_chars('payload', 50, 10),
+    missile_chars('time2target', 100, 50)
+]
+
+missile_2 = [
+    missile_chars('traj',100,50),
+    missile_chars('cost', 100, 50),
+    missile_chars('payload', 50, 10),
+    missile_chars('time2target', 100, 50)
+]
+
+missile_3 = [
+    missile_chars('traj',100,50),
+    missile_chars('cost', 100, 50),
+    missile_chars('payload', 50, 10),
+    missile_chars('time2target', 100, 50)
+]
+
+missile_4 = [
+    missile_chars('traj',100,50),
+    missile_chars('cost', 100, 50),
+    missile_chars('payload', 50, 10),
+    missile_chars('time2target', 100, 50)
+]
