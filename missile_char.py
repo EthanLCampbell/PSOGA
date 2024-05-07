c9c4327994ec6580d53e5ce79a0cfc41e8a93923
Nick <nicholas.canovas@gmail.com>
2024-05-07 16:05:23 -0400

    Changed missile characteristics based on geostrat document (will link to git)

--- missile_char.py
+++ missile_char.py
@@ -3,30 +3,82 @@ from typing import List
 
 missile_chars = namedtuple('missile_chars', ['name', 'value', 'weight'])
 
-missile_1 = [
-    missile_chars('traj',100,50),
-    missile_chars('cost', 100, 50),
-    missile_chars('payload', 50, 10),
-    missile_chars('time2target', 100, 50)
+tomahawk_Biv = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
 ]
 
-missile_2 = [
-    missile_chars('traj',100,50),
-    missile_chars('cost', 100, 50),
-    missile_chars('payload', 50, 10),
-    missile_chars('time2target', 100, 50)
+storm_shadow = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
 ]
 
-missile_3 = [
-    missile_chars('traj',100,50),
-    missile_chars('cost', 100, 50),
-    missile_chars('payload', 50, 10),
-    missile_chars('time2target', 100, 50)
+agm_harm = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
 ]
 
-missile_4 = [
-    missile_chars('traj',100,50),
-    missile_chars('cost', 100, 50),
-    missile_chars('payload', 50, 10),
-    missile_chars('time2target', 100, 50)
+p800_rus = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
+]
+
+zircon_rus = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
+]
+
+df_zf_china = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
+]
+
+avangard_rus = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 2, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
+]
+
+c_hcb = [
+    missile_chars('top_speed',0.7, 100), #altitude
+    missile_chars('range', 1000, 100), #moneys
+    missile_chars('manouv_radius', 10, 100), #turn radius, ft
+    missile_chars('manouv_rate', 10, 100), #turn rate, degree/sec
+    missile_chars('cost', 41, 50), #million usd
+    missile_chars('precision', 100, 50), #ft^2
+    missile_chars('payload', 100, 50) #lbs
 ]
