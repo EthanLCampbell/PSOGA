486ce753f511044a49b1ceebea83121eafa6edd6
EthanLCampbell <ethan@labianca-campbell.com>
2024-05-11 11:05:42 -0700

    edits - trying to add animation to the fishes

--- PSO_main.py
+++ PSO_main.py
@@ -2,7 +2,7 @@
 # 
 # Ethan Labianca-Campbell - Purdue MSAA '26 
 # Particle Swarm Optimization (PSO) Framework with Python
-# May, 2024 
+# Last Update: May 10, 2024 
 # 
 #------------------------------------------------------------------------------+
 
@@ -12,6 +12,9 @@ import numpy as np
 import random
 import math
 from pso.cost_functions import sphere #another function to check with
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
 
 #---COST FUNCTION -------------------------------------------------------------+
 
@@ -45,15 +48,15 @@ class Particle:
         self.error_i = costFunction(self.position_i)
         #check if particle position is individual best
         if self.error_i < self.error_best_i or self.error_best_i == -1:
-            self.best_position_i = self.position_i
+            self.best_position_i = self.position_i.copy()
             self.error_best_i = self.error_i
 
     #update new particle velocity 
     def update_velocity(self,group_best_position):
         #Search constants
         inertia = 0.5   # intertia weight (tendency toward last velocity)
-        cog = 1       # cognitive weight (tendency toward individual best)
-        soc = 1       # social weight (tendency toward global best)
+        cog = 1      # cognitive weight (tendency toward individual best)
+        soc = 0.8       # social weight (tendency toward global best)
 
         for i in range (0,num_dimensions):
             r1 = random.random() #random permutation on cognitive
@@ -69,7 +72,7 @@ class Particle:
             self.position_i[i] = self.position_i[i] + self.velocity_i[i]
 
             #new maximum position bound if necessary
-            if self.position_i[i]>bounds[i][0]:
+            if self.position_i[i]>bounds[i][1]:
                 self.position_i[i] = bounds[i][1]
             
             #new minimum position bound if necessary
@@ -81,6 +84,8 @@ class Particle:
 class PSO():
     def particle_swarm(costFunction,x0,bounds,num_particles,maxiter):
         global num_dimensions #number for total 
+        global xdata
+        global ydata
 
         num_dimensions = len(x0)
         group_error_best = -1            #best error for the group
@@ -93,6 +98,8 @@ class PSO():
 
         #optimization loop
         i = 0
+        xdata = []
+        ydata = []
         while i<maxiter:
             #print i, group_error_best
             print(f'iter: {i:>4d}, best solution: {group_error_best:10.6f}')
@@ -110,18 +117,54 @@ class PSO():
                 swarm[j].update_velocity(group_best_position)
                 swarm[j].update_position(bounds)
             
+            #make globals of positions to animate:
+            for j in range(0,num_particles):
+                xdata.append(swarm[j].position_i[0])
+                ydata.append(swarm[j].position_i[1])
+
             #loop again
             i+=1
+       
+       
+
         #print final results
         print('Final:')
         print(f'   > {group_position_best}')
         print(f'   > {group_error_best}\n')
 
         return group_error_best, group_position_best
+    
 #---RUN------------------------------------------------------------------------+    
 
 x0 = [5,5]                   #starting location [x1,x2,x3,....]
 bounds = [(-10,10),(-10,10)] #bounds for search [(x1min,x1max),(x2min,x2max),..]
-PSO.particle_swarm(sphere,x0,bounds,num_particles=20,maxiter=11)
+num_particles = 20           #number of swarm particles
+maxiter = 30                 #number of steps
+PSO.particle_swarm(func1,x0,bounds,num_particles,maxiter)
+
+#---ANIMATIONS-(WIP)------------------------------------------------------------+    
+
+#generate figure 
+fig, ax = plt.subplots()
+ax = plt.axes(xlim = bounds[0],ylim = bounds[1]) #change to be defined by bounds
+n = num_particles
+ln, = plt.plot([],[],'ro',lw=2)
+
+#def init():
+#    ax.set_xlim(-1,1)
+#    ax.set_ylim(-1,1)
+#    return ln,
+
+def update(frame):
+    xdata_ani = xdata[frame]
+    ydata_ani = ydata[frame]
+    #ln.set_data(xdata_ani,ydata_ani)
+    plt.plot(xdata[frame],ydata[frame],'ro')
+    return ln,
+
+ani = FuncAnimation(fig,update,frames=len(xdata),interval=1000)
+plt.show()
+#ani.save('swarm_ani.gif',writer='pillow')
 
 #---END------------------------------------------------------------------------+    
+
